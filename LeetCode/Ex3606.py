1b6ff0e4c103e6b8a37967cbb17e682412b56dd9
RayanBrenno <rayanbrennodev@gmail.com>
2026-01-14 17:17:28 -0300

    done

---
+++ LeetCode/Ex3606.py
@@ -0,0 +1,24 @@
+from typing import List
+def validateCoupons(self, code: List[str], businessLine: List[str], isActive: List[bool]) -> List[str]:
+    lines = ["electronics", "grocery", "pharmacy", "restaurant"]
+    aux = [[] for x in range(len(lines))]
+    check = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"
+    for x in range(len(code)):
+        if isActive[x]:
+            if businessLine[x] in lines: 
+                if all(c == "_" or c.isalnum() for c in code[x]) and len(code[x]) > 0:
+                    aux[lines.index(businessLine[x])].append(code[x])
+                    
+    ans = []
+    
+    for x in aux:
+        sorted(x)
+        if len(x) > 0:
+            ans.extend(x)
+    return ans
+
+code = ["1OFw","0MvB"]
+businessLine = ["electronics","pharmacy"]
+isActive = [True,True]
+print(validateCoupons(None, code, businessLine, isActive))
+    
