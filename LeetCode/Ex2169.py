f24e112bb44551c131fdd69390f0cb707c0d572f
RayanBrenno <rayanbrennodev@gmail.com>
2026-02-20 17:21:22 -0300

    done

--- LeetCode/Ex2169.py
+++ LeetCode/Ex2169.py
@@ -1,3 +1,5 @@
+# O código conta quantas operações de subtração seriam feitas entre dois números até que um deles vire zero. Enquanto os dois números forem diferentes de zero (num1 * num2 != 0), ele sempre pega o maior e verifica quantas vezes o menor cabe dentro dele usando divisão inteira (//). Esse valor representa quantas subtrações poderiam ser feitas de uma vez só, então ele soma isso em ans. Depois, atualiza o número maior com o resto da divisão (%), que equivale ao que sobraria após essas subtrações. O processo continua até que um dos números vire zero, e o total acumulado em ans é retornado.
+
 def countOperations(self, num1: int, num2: int) -> int:     
     # res = 0
     # while num1*num2 != 0:
