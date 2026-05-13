6c45e6ddbc002808f5c8aa6acb9dcacbc0f573aa
RayanBrenno <rayanbrennodev@gmail.com>
2026-05-13 17:50:36 -0300

    feat(leetcode): add solution for problem 2177

---
+++ LeetCode/Ex2177.py
@@ -0,0 +1,12 @@
+# O código verifica se é possível dividir num em 3 números consecutivos cuja soma seja igual a ele. Para isso, testa se (num - 3) é divisível por 3. Se for, calcula o primeiro número x e retorna [x, x + 1, x + 2]. Caso contrário, retorna uma lista vazia porque não existe sequência consecutiva válida.
+
+def sumOfThree(self, num: int) -> List[int]:
+    if (num - 3) % 3 == 0:
+        x = (num - 3) // 3
+        return [x, x + 1, x + 2]
+    else:
+        return []
+
+
+num = 33
+print(sumOfThree(num))  # Output: [10, 11, 12]
