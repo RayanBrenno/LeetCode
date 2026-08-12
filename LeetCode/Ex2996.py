209d36449ec13848a0e4c02d402872c7acfbb9a7
Rayan Brenno <rayanbrennodev@gmail.com>
2026-08-11 21:36:43 -0300

    fix(leetcode): correct List import in problem 2996 solution

--- LeetCode/Ex2996.py
+++ LeetCode/Ex2996.py
@@ -1,6 +1,6 @@
 # A implementação começa calculando a soma do maior prefixo consecutivo presente no início do array, começando por nums[0] e percorrendo os elementos enquanto cada valor for exatamente o anterior mais 1. Quando a sequência consecutiva é interrompida, o algoritmo para e transforma o array em um set para permitir verificações mais rápidas. Em seguida, enquanto a soma calculada já estiver presente no conjunto, ela é incrementada em 1. Assim, ao final, retornamos o menor inteiro que é maior ou igual à soma do prefixo consecutivo e que não aparece em nums.
 
-from typing import Listw
+from typing import List
 
 def missingInteger(self, nums: List[int]) -> int:
     total = nums[0]
